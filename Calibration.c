02240d480adc32eb604f075d40eab789b45a6a2f
DonggyunL <dlehdrbs729@korea.ac.kr>
2022-09-08 16:57:40 +0900

    test commit

---
+++ Calibration.c
@@ -0,0 +1,22 @@
+// library import
+#include <stdio.h>
+
+// initializing
+int main(){
+    printf("start?\n");
+    return 0;
+}
+
+// connect handheld camera
+
+
+// capture vedio frame
+
+
+// detect checker board
+
+
+// camera calibration
+
+
+// store camera matrix to file and export
